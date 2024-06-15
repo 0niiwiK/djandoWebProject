b0f3c83701af76c364d5b5224199e1df0e8c495f
0niiwiK <115632110+0niiwiK@users.noreply.github.com>
2024-06-15 21:34:01 +0200

    Added almost all

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -111,13 +111,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-# Media files (Uploaded files)
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
 
 # Asegúrate de tener las siguientes líneas si no están ya presentes
 STATICFILES_DIRS = [
