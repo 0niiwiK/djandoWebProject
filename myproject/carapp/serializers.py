f76041a8d9867eda3d919906db65d8e9b927f0b2
0niiwiK <115632110+0niiwiK@users.noreply.github.com>
2024-06-16 14:40:39 +0200

    Added almost all

--- myproject/carapp/serializers.py
+++ myproject/carapp/serializers.py
@@ -7,20 +7,22 @@ class CarImageSerializer(serializers.ModelSerializer):
         fields = ['image']
 
 class CarSerializer(serializers.ModelSerializer):
-    images = serializers.ListField(
-        child=serializers.ImageField(max_length=1000000, allow_empty_file=False, use_url=False)
-    )
+    images = CarImageSerializer(many=True, read_only=True)
     phone_number = serializers.CharField(write_only=True)
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(max_length=1000000, allow_empty_file=False, use_url=False),
+        write_only=True
+    )
 
     class Meta:
         model = Car
-        fields = ['license_plate', 'phone_number', 'images']
+        fields = ['license_plate', 'phone_number', 'images', 'uploaded_images']
 
     def create(self, validated_data):
-        images_data = validated_data.pop('images')
+        uploaded_images_data = validated_data.pop('uploaded_images')
         phone_number = validated_data.pop('phone_number')
         user = CustomUser.objects.get(phone_number=phone_number)
         car = Car.objects.create(uploaded_by=user, **validated_data)
-        for image_data in images_data:
+        for image_data in uploaded_images_data:
             CarImage.objects.create(car=car, image=image_data)
         return car
