fb7f5bb0cb803124531d2ff31bec8444257b8cc8
0niiwiK <115632110+0niiwiK@users.noreply.github.com>
2024-06-16 14:58:48 +0200

    Added almost all

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -46,6 +46,17 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.MultiPartParser',
+        'rest_framework.parsers.FormParser',
+    ),
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
