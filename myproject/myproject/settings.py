d4ba4b57d4a187a1c0992eeccc34c03458e2fcb5
0niiwiK <115632110+0niiwiK@users.noreply.github.com>
2024-06-17 10:24:43 +0200

    Minimal Fixes

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -97,14 +97,27 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'djangodb',
+
+        'USER': 'kiwii',
+
+        'PASSWORD': 'ride1234',
+
+        'HOST': 'localhost',
+
+        'PORT': '<db_port>',
+
     }
+
 }
-
-
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'search'
